529aa54bc1830c66d964a4ca9ae83725e5a5e050
LeoDeschaux <leo.db40@gmail.com>
2021-06-15 17:48:51 +0200

    Ajout de la vérification de l'input de l'utilisateur
    isAdressValid permet de vérifier si l'input est une adresse ip, et si les valeurs son bonnes

--- main.c
+++ main.c
@@ -38,40 +38,135 @@ int isAddressValid(char* input);
 
 int main(int argc, const char** argv)
 {
-	//READ INPUT, ex: "192.168.1.13/24"
-	
-	char ip[50];
-	scanf("%s", ip);
-	printf("voici l'adresse : %s\n", ip);
+	while (true)
+	{
+		//READ INPUT, ex: "192.168.1.13/24"
+		char ip[50]; //= "192.168.1.9/24";
+		scanf("%s", ip);
+		printf("voici l'adresse : %s\n\n", ip);
+		
+		//CHECK IF INPUT IS VALID
+		if (isAddressValid(&ip))
+			printf("address is valid\n");
+		else
+			printf("adress n'est pas valide..\n");
 
-	//CHECK IF INPUT IS VALID
+		//CONVERT INPUT INTO IP STRUCT
 
-	if (isAddressValid(&ip))
-		printf("address is valid\n");
-	
-	//CONVERT INPUT INTO IP STRUCT
-		//convert char to int
+		//FIND IP CLASS (A,B,C) & public/private
 
-	//FIND IP CLASS (A,B,C) & public/private
-	
-	//FIND @network, @host
+		//FIND @network, @host
 
-	//PRINT IP & save in file 
+		//PRINT IP & save in file 
+
+		printf("-------------------------------------------\n");
+	}
+
+	printf("\n");
 
 	return 0;
 }
 
 int isAddressValid(char* input)
 {
+	enum state { record_valeur, record_mask };
+	enum state state = record_valeur;
+
+	char valeurs[4][3 + 1] = {"", "xxx", "xxx", "xxx"};
+	int valeurIndex = 0; 
+	char mask[3] = "xx";
+
+	int longueurValeur = 0;
+
+	bool isValid = false;
+
 	int i = 0;
 	while (*(input + i) != '\0' && i < 20)
 	{
-		printf("%c", *(input + i));
+		char currentChar = { *(input + i) };
+
+		if (state == record_valeur)
+		{
+			if (isdigit(currentChar))
+			{
+				if (longueurValeur >= 3)
+					return false;
+
+				strncat(valeurs[valeurIndex], &currentChar, 1);
+				longueurValeur++;
+			}
+			else if (currentChar == '.')
+			{
+				if (longueurValeur == 0)
+					return false;
+
+				if (valeurIndex < 3)
+				{
+					valeurIndex++;
+					memset(valeurs[valeurIndex], 0, sizeof(valeurs[valeurIndex]));
+					longueurValeur = 0;
+				}
+				else
+					return false;
+			}
+			else if (valeurIndex == 3 && currentChar == '/')
+			{
+				state = record_mask;
+				memset(mask, 0, sizeof(mask));
+				longueurValeur = 0;
+			}
+			else
+				return false;
+		}
+		
+		else if (state == record_mask)
+		{
+			if (isdigit(currentChar))
+			{
+				if (longueurValeur >= 2)
+					return false;
+
+				strncat(mask, &currentChar, 1);
+				longueurValeur++;
+			}
+			else
+				return false;
+		}
+		
 		i++;
 	}
-	printf("\n");
 
-	return 0;
+	//CHECK IF ALL FIELDS HAVE BEEN SET
+	if (strcmp(valeurs[0], "xxx") == 0 ||
+		strcmp(valeurs[1], "xxx") == 0 ||
+		strcmp(valeurs[2], "xxx") == 0 ||
+		strcmp(valeurs[3], "xxx") == 0 ||
+		strcmp(mask, "xx") == 0)
+	{
+		return false;
+	}
+
+	//CHECK IF VALUES ARE VALID
+	if (atoi(valeurs[0]) <= 255 &&
+		atoi(valeurs[1]) <= 255 &&
+		atoi(valeurs[2]) <= 255 &&
+		atoi(valeurs[3]) <= 255 &&
+		atoi(mask) <= 32)
+	{
+		isValid = true;
+	}
+
+	//DEBUG: PRINT @IP
+	printf("----------------------- \n");
+	printf("Resultats\n");
+	printf("VALEUR: %s\n", valeurs[0]);
+	printf("VALEUR: %s\n", valeurs[1]);
+	printf("VALEUR: %s\n", valeurs[2]);
+	printf("VALEUR: %s\n", valeurs[3]);
+	printf("MASK: %s\n",   mask);
+	printf("---------------- \n\n");
+
+	return isValid;
 }
 
 char* getInput()
